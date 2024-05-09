a4a96adc5fa1a94bb88020d63cbc654f94657460
datacvriskypratama <81645433+datacvriskypratama@users.noreply.github.com>
2024-05-10 03:08:22 +0700

    commit

---
+++ index.php
@@ -0,0 +1,57 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>WEB - OFFICIAL FAMILY FSM</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  </head>
+  <body>
+    <br>
+    <!-- NAVBAR -->
+    <nav class="navbar navbar-expand-lg" style="background-color: #32B5E7; box-shadow: 2px 3px 4px 1px;">
+<div class="container-fluid">
+  <a class="navbar-brand" href="#"><b>PROFIL AHMAD</b></a>
+  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+    <div class="navbar-nav ms-auto">
+      <a class="nav-link active" aria-current="page" href="#">𝐁𝐞𝐫𝐚𝐧𝐝𝐚</a>
+      <a class="nav-link" href="https://www.tiktok.com/@akbar.asapil1">𝐓𝐢𝐤-𝐓𝐨𝐤</a>
+      <a class="nav-link" href="https://www.facebook.com/profile.php?id=100083772122314">𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤</a>
+      <a class="nav-link disabled" aria-disabled="true">#</a>
+    </div>
+  </div>
+</div>
+</nav>
+    <!-- END NAVBAR -->
+
+    <!-- JUMBOTRON -->
+            <div class="jumbotron" style="background-color: #F4BD02; box-shadow: 2px 3px 4px;">
+              <br>
+              <br>
+              <center><img src="https://scontent.fmdc3-1.fna.fbcdn.net/v/t39.30808-6/411591876_326564296812639_1523872492423740556_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeEhSWBeCrPqug49GFCn1P1mLoKLTOpNVYUugotM6k1VhXVMYFXyHREL3QmXaVIcp9OCE3j0XUCEr7_BQZnzvWrZ&_nc_ohc=8JSd1MSTyzQQ7kNvgHrlbck&_nc_ht=scontent.fmdc3-1.fna&oh=00_AYCPV73-XbnKC83gWyre1s1BHHAgvqmnnszg60SSbk56ZQ&oe=6642EA52" alt="logo family" style="border-radius: 50%;" width="10%"></center>
+          <h3 class="display-8" style="padding-top: 5px;"><center>𝗔𝗵𝗺𝗮𝗱 𝗔𝘀𝗮𝗽𝗶𝗹 <img src="img/png-transparent-blue-check-logo-social-media-instagram-verified-badge-symbol-computer-icons-social-media-blue-leaf-influencer-marketing-removebg-preview.png" alt="resmi centang" width="2%"></center></h3>
+          <hr class="my-4" style="box-shadow: 5px 5px 5px">
+          <br>
+          <br>
+          <br>
+          <br>
+          <h3><p><center>𝗦𝗢𝗖𝗜𝗔𝗟 𝗠𝗘𝗗𝗜𝗔</center></p></h3>
+          <br>
+          <center><img src="img/Capture3-removebg-preview.png" alt="TENTANG FAMILY" width="25%" style="box-shadow: 2px 3px 4px; background-color: #fff; border-radius: 5%"><img src="img/Tanpa_judul-removebg-preview (1).png" alt="" width="50%"></center>
+          <br>
+          <br>
+          <br>
+        </div>
+    <!-- END JUMBOTRON -->
+    <!-- FOOTER -->
+          <footer style="background-color: #32B5E7; box-shadow: 2px 3px 4px 1px;">
+        <h3><a><center><b><center>© ❤️AHMAD PROFILE, 𝟐𝟎𝟐𝟒/𝟐𝟎𝟐𝟓</b></center></a></h3>
+      </footer>
+    <!-- END FOOTER -->
+    <br>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  </body>
+</html>
